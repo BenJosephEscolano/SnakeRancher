730e3b46cfd6b40c461d0d57b3ec7bb0b0158c57
TheMetalPipe <erojorichimir@gmail.com>
2025-03-27 12:35:15 +0800

    Add files via upload

--- app/src/main/java/com/csit284/snakerancher/ProfileActivity.kt
+++ app/src/main/java/com/csit284/snakerancher/ProfileActivity.kt
@@ -1,30 +1,30 @@
 package com.csit284.snakerancher
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ProfileActivity : Activity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-        //Placeholder
+        setContentView(R.layout.profile)
 
-        val gotosettings = findViewById<TextView>(R.id.go_to_settings)
-        val gotoLeaderBoard = findViewById<TextView>(R.id.go_to_leaderboard)
-        gotosettings.setOnClickListener(){
-            val intent = Intent(this, SettingActivity::class.java)
+        val button_ranking = findViewById<Button>(R.id.button_ranking)
+        val imageview_exit = findViewById<ImageView>(R.id.imageview_exit)
+
+        button_ranking.setOnClickListener() {
+            val intent = Intent(this, LeaderboardActivity::class.java)
             startActivity(intent)
         }
-        gotoLeaderBoard.setOnClickListener(){
-            val intent = Intent(this, LeaderboardActivity::class.java)
+
+        imageview_exit.setOnClickListener() {
+            val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
     }
